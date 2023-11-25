8c707b4aa4dcebde273498c84a53cf2fc63574c4
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-11-25 09:48:44 +0530

    chart added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,54 @@
-import './App.css';
-import {io} from 'socket.io-client'
-import { useEffect, useState } from 'react';
-import LastTradedPrice from './components/LastTradedPrice';
-import CandlestickChart from './components/CandlestickChart';
+import "./App.css";
+import { io } from "socket.io-client";
+import { useEffect, useState } from "react";
+import LastTradedPrice from "./components/LastTradedPrice";
+import CandlestickChart from "./components/CandlestickChart";
 
 function App() {
+  const [ltpData, setLtpData] = useState({});
+  const [candleStickData, setCandleStickData] = useState([]);
+  const [instrument, setInstrument] = useState("Nifty");
+  const [candlestickinterval, setCandlestickinterval] = useState(1);
 
-  const [ltpData,setLtpData] = useState({});
-  const [candleStickData,setCandleStickData] = useState([])
-  
-  useEffect(()=>{
-    const socket = new WebSocket('wss://functionup.fintarget.in/ws?id=fintarget-functionup');
-  
-    socket.addEventListener('open',()=>{
-      console.log('Connected to WebSocket');
-    })
+  useEffect(() => {
+    const socket = new WebSocket(
+      "wss://functionup.fintarget.in/ws?id=fintarget-functionup"
+    );
 
-    socket.addEventListener('message',(event)=>{
+    socket.addEventListener("open", () => {
+      console.log("Connected to WebSocket");
+    });
+
+    socket.addEventListener("message", (event) => {
       // console.log(event)
-      // console.log(event.data);
-      const ltp = JSON.parse(event.data)
-      setLtpData(ltp)
-      setCandleStickData((prev)=>[...prev,ltp])
-    })
-     return () => {
+      const ltp = JSON.parse(event.data);
+      setLtpData(ltp);
+      setCandleStickData((prev) => [...prev, ltp]);
+    });
+    return () => {
       // socket.disconnect();
       socket.close();
-     }
-  },[])
-
-
-
+    };
+  }, []);
 
   return (
-    <div className="App" >
-
+    <div className="App">
       <LastTradedPrice ltp={ltpData} />
-      <CandlestickChart data={candleStickData} interval={1} instrument="Nifty"/>
-      <CandlestickChart data={candleStickData} interval={3} instrument="Nifty"/>
-      <CandlestickChart data={candleStickData} interval={5} instrument="Nifty"/>
+      <h1 style={{color:"white"}}>CandlestickChart</h1>
+      <label style={{color:"white"}}>
+        Select Instrument : {" "}
+        <select
+         
+          style={{ padding: "10px" ,marginBottom:"10px"}}
+          value={instrument}
+          onChange={(e) => setInstrument(e.target.value)}
+        >
+          <option value={"Nifty"}>Nifty</option>
+          <option value={"Banknifty"}>BankNifty</option>
+          <option value={"Finnifty"}>FinNifty</option>
+        </select>
+      </label>
+      <CandlestickChart data={ltpData} instrument={instrument} />
     </div>
   );
 }
--- frontend/src/components/CandlestickChart.js
+++ frontend/src/components/CandlestickChart.js
@@ -1,92 +1,61 @@
-// import React, { Component } from 'react';
-// import CanvasJSReact from '@canvasjs/react-charts';
-// //var CanvasJSReact = require('@canvasjs/react-charts');
- 
-// var CanvasJS = CanvasJSReact.CanvasJS;
-// var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-// var dataPoints =[];
-// class CandlestickChart extends Component {
-//   constructor(props) {
-//     super(props);
-//     let mappedData = props.data.map((el)=>{
-//       // console.log(el.Nifty)
-//     })
-//     // console.log(props.data)
-// }
-
-// render() {
-// const options = {
-// exportEnabled: true,
-// title: {
-// text: "Algowiz"
-// },
-// axisX: {
-// valueFormatString: "D MMM"
-// },
-// axisY: {
-// title: "Price",
-// prefix: "$"
-// },
-// data: [{
-// type: "candlestick",
-// name: "Algowiz",
-// showInLegend: true,
-// yValueFormatString: "$##0.00",
-// xValueType: "dateTime",
-// dataPoints: dataPoints
-// }]
-// }
-// return (
-// <div>
-// <CanvasJSChart options = {options} onRef={ref => this.chart = ref}/>
-// </div>
-// );
-// }
-// componentDidMount(){
-// var chart = this.chart;
-// fetch('https://canvasjs.com/data/gallery/react/microsoft-stock-price.json')
-// .then(function(response) {
-// return response.json();
-// })
-// .then(function(data) {
-// for (var i = 0; i < data.length; i++) {
-// dataPoints.push({
-// x: data[i].x,
-// y: data[i].y
-// });
-// }
-// chart.render();
-// });
-// }
-// }
-// export default CandlestickChart;
+import React, { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
 
 
-import React from 'react'
-import Chart from 'react-apexcharts'
-const CandlestickChart = ({data,interval,instrument}) => {
-  const info = [
-    {
-      data : data.map(item=>({
-        x : interval,
-        y : [item.open, item.high, item.low, item.close],
-      }))
-    }
-  ]
+const CandlestickChart = ({ data, instrument = "Nifty" }) => {
+  const [series, setSeries] = useState([{ data: [] }]);
+  const [graphData, setGraphData] = useState([]);
+
+  useEffect(() => {
+    let n = 1;
+
+    setGraphData((prev) => [
+      ...prev,
+      {
+        x: new Date().getTime() + (60000 * (n * (n + 1))) / 2,
+        y: [
+          data[instrument] + Math.random() + 20,
+          data[instrument] + Math.random() + 20,
+          data[instrument] + Math.random() + 20,
+          data[instrument] + Math.random() + 20,
+        ],
+      },
+    ]);
+
+    n++;
+
+  }, [data[instrument], instrument]);
+
+  useEffect(() => {
+    setSeries([{ data: graphData }]);
+  }, [graphData.length]);
+
   const options = {
-    chart : {
-      type : 'candlestick'
+    chart: {
+      type: "candlestick",
     },
-    xaxis: {
-      type : 'datetime'
-    }
-  }
-  return (
-    <div>
-      <h2 style={{color:"white"}}>{instrument}</h2>
-      <Chart options={options} series={info} type="candlestick" height={350} />
-    </div>
-  )
-}
 
-export default CandlestickChart
+    xaxis: {
+      type: "datetime",
+    },
+
+    yaxis: {
+      tooltip: {
+        enabled: true,
+      },
+    },
+  };
+
+  return (
+    <div style={{ background: "white" ,padding:"10px",}}>
+      <Chart
+        options={options}
+        series={series}
+        type="candlestick"
+        height={350}
+      />
+    </div>
+  );
+};
+
+export default CandlestickChart;
